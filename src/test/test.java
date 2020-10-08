ea4928d2d6015f7cfa66e907d9db763113e526b9
subhamstark <subham@sap.com>
2020-10-08 09:33:10 +0530

    UC7 Done

---
+++ src/test/test.java
@@ -0,0 +1,25 @@
+package test;
+
+import main.LinkedList;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class test {
+    LinkedList<Integer> list = new LinkedList<>();
+
+    @Before
+    public void initialize() {
+        list.addNodeLast(59);
+        list.addNodeLast(70);
+        list.addNodeLast(30);
+    }
+
+
+    @Test
+    public void givenMaxNumber_At1stPosition_ReturnNumber() {
+        assertEquals(true,list.findNodeWithValue(30));
+    }
+
+}
